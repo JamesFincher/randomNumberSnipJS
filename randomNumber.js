e6dafa8e2fe798a7acd753bcd89cd03249d5c02a
heartandkarma <heartandkarma@gmail.com>
2022-01-12 14:32:37 -0800

    init commit

---
+++ randomNumber.js
@@ -0,0 +1,7 @@
+randomNumber = (highNum) => Math.floor(Math.random() * highNum + 1);
+
+//
+//sample for loop that consol logs 100 random numbers between 1 and 1000
+//for (i = 0; i < 100; i++) {
+//   console.log(randomNumber(1000));
+// }
